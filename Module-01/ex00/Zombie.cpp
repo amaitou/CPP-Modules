35ae1ccd195cee97d154fb222db0580ed1f3e93b
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-07-28 19:14:56 +0100

    added some needed files to separate work

--- Module-01/ex00/Zombie.cpp
+++ Module-01/ex00/Zombie.cpp
@@ -14,15 +14,3 @@ void Zombie::announce(void)
 {
 	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
-
-Zombie *newZombie(std::string name)
-{
-	Zombie	*new_zombie = new Zombie(name);
-	return new_zombie;
-}
-
-void randomChump(std::string name)
-{
-	Zombie chump(name);
-	chump.announce();
-}
---
+++ Module-01/ex00/randomChump.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+void randomChump(std::string name)
+{
+	Zombie chump(name);
+	chump.announce();
+}
