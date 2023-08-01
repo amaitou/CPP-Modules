13a40c6a0ef1da5e069b9e06ae30d9e8687b4674
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-08-02 00:55:44 +0100

    done with CPP01/ex04

---
+++ Module-01/ex04/main.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string filename;
+	std::string string1;
+	std::string string2;
+	std::string replaced;
+	std::fstream fileone; 
+	std::fstream filetwo;
+	std::string  buffer;
+	size_t position;
+	
+	std::cout << "[.] Enter The FileName: ";
+	std::cin >> filename;
+	fileone.open(filename, std::ios::in);
+	if (!fileone.is_open())
+	{
+		std::cout << "[!] Could not open the given file" << std::endl;
+		return (0);
+	}
+	replaced = filename + ".replaced";
+	filetwo.open(replaced, std::ios::out);
+	if (!filetwo.is_open())
+	{
+		std::cout << "[!] Could not open the replaced file" << std::endl;
+		return (0);
+	}
+	std::cout << "[.] Enter The String1: ";
+	std::cin >> string1;
+	std::cout << "[.] Enter The String2: ";
+	std::cin >> string2;
+	
+	while (getline(fileone, buffer, '\n'))
+	{
+		position = 0;
+		while ((position = buffer.find(string1, position)) != std::string::npos)
+			buffer = buffer.substr(0, position) + string2 + buffer.substr(position + string1.length(), buffer.length());
+		filetwo << buffer << "\n";
+	}
+	return (0);
+}
