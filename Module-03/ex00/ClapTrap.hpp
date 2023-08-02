cc908a25680bd6a0dbcb85b3a8d7c1f5155661cd
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-08-02 07:44:19 +0100

    done with CPP03/ex00

---
+++ Module-03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string _name;
+		unsigned int	_health;
+		unsigned int	_energy;
+		unsigned int _attack;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &copy);
+		ClapTrap & operator = (const ClapTrap &object);
+		~ClapTrap();
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+} ;
+
+#endif
---
+++ Module-03/ex00/main.cpp
@@ -0,0 +1,24 @@
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap clapone   = ClapTrap();
+	ClapTrap claptwo   = ClapTrap("Warrior_1");
+
+	clapone.takeDamage(5);
+	clapone.beRepaired(30);
+	clapone.attack("l3do");
+
+	claptwo.attack("l3do_2");
+	claptwo.takeDamage(10);
+	claptwo.beRepaired(50);
+
+	ClapTrap clapthree;
+
+	clapthree = claptwo;
+	clapthree.attack("l3do_3");
+	clapthree.takeDamage(4);
+	clapthree.beRepaired(20);
+	return (0);
+}
