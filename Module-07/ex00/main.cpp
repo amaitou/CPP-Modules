61595ab014519ab6864420b4dd96fceb0eddfcec
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-10-18 18:41:02 +0100

    done with CPP07EX00

---
+++ Module-07/ex00/main.cpp
@@ -0,0 +1,45 @@
+#include "whatever.hpp"
+
+int main(void)
+{
+	// My Tests
+	std::cout << "My Tests :" << std::endl;
+	{
+		// Swap Function
+		int a = 5;
+		int	b = 10;
+		std::cout << "a :" << a << ", b: " << b << std::endl;
+		swap(a, b);
+		std::cout << "a :" << a << ", b: " << b << std::endl;
+
+		// Max Function
+		char x = 'x';
+		char y = 'y';
+		std::cout << max(x, y) << std::endl;
+		std::cout << max(y, x) << std::endl;
+
+		// Min Function
+		float f1 = 127.245f;
+		float f2 = 117.243f;
+		std::cout << min(f1, f2) << std::endl;
+	}
+
+	// Default Exercice Tests
+	std::cout << "\nDefault Tests :" << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+		swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		return 0;
+	}
+	return (EXIT_SUCCESS);
+}
---
+++ Module-07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T t;
+
+	t = a;
+	a = b;
+	b = t;
+}
+template <typename T>
+T	max(T &a, T &b)
+{
+	return (a > b)? a : b;
+}
+
+template <typename T>
+T	min(T &a, T &b)
+{
+	return (a < b)? a : b;
+}
+#endif
