d346127ebb734f385b501afae4c491f640801c79
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-10-19 02:58:11 +0100

    done with CPP07EX02

---
+++ Module-07/ex02/Array.hpp
@@ -0,0 +1,88 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+typedef unsigned int t_ui;
+template <typename T>
+class Array
+{
+	private:
+		T *array;
+		t_ui n;
+	public:
+		Array(void)
+		{
+			this->array = new T();
+		}
+		Array(t_ui n)
+		{
+			this->array = new T[n];
+			this->n = n;
+		}
+		~Array()
+		{
+			delete[] this->array;
+		}
+		Array(const Array &copy)
+		{
+			this->n = copy.size;
+			this->array = new T[copy.size];
+			size_t i(0);
+			while (i < this->n)
+			{
+				this->array[i] = copy.array[i];
+				++i;
+			}
+		}
+		class IndexOutOfRange: public std::exception
+		{
+			public:
+				const char *what() const throw()
+				{
+					return "Index Out Of Range";
+				}
+		};
+		Array & operator = (const Array &object)
+		{
+			this->n = object.n;
+			this->array = new T[object.n];
+			size_t i(0);
+			while (i < object.n)
+			{
+				this->array[i] = object.array[i];
+				++i;
+			}
+			return (*this);
+		}
+		T & operator[](t_ui index) const
+		{
+			if (index >= this->n)
+				throw Array::IndexOutOfRange();
+			return (this->array[index]);
+		}
+		t_ui	size(void) const
+		{
+			return (this->n);
+		}
+		
+};
+
+template <typename T>
+std::ostream & operator << (std::ostream &os, const Array<T> &arr)
+{
+	os << "[ ";
+	size_t i(0);
+	while (i < arr.size())
+	{
+		os << arr[i];
+		if (!(i + 1 == arr.size()))
+			os << ", ";
+		++i;
+	}
+	os << "]";
+	return (os);
+}
+
+#endif
---
+++ Module-07/ex02/main.cpp
@@ -0,0 +1,74 @@
+#include "Array.hpp"
+
+int main(void)
+{
+    // test the array with integers
+	std::cout << "Test With Int:" << std::endl;
+    {
+		// create an instance
+		try
+		{
+			Array<int> integers_1(10);
+			Array<int> integers_2;
+			t_ui i(0);
+
+			srand(time(NULL));
+			while (i < integers_1.size())
+			{
+				// fill the array with random numbers
+				integers_1[i] = (rand() % 20) + 1;
+				++i;
+			}
+			// print array elements of integer_1
+			std::cout << integers_1 << std::endl;
+
+			// using copy assignment operator to assign integer_1 to integer_2
+			integers_2 = integers_1;
+			// print array elements of integer_2
+			std::cout << integers_2 << std::endl;
+
+			// try to access with a valid index
+			std::cout << integers_2[5] << std::endl;
+			// try to access with an index out of range
+			std::cout << integers_1[100] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+    }
+	// test the array with strings
+	std::cout << "\nTest With Strings:" << std::endl;
+	{
+		try
+		{
+			Array<std::string> strings_1(5);
+			Array<std::string> strings_2;
+
+			// fill the array 
+			strings_1[0] = "Alice";
+			strings_1[1] = "BOb";
+			strings_1[2] = "Charlie";
+			strings_1[3] = "David";
+			strings_1[4] = "Eve";
+
+			// print the array elements
+			std::cout << strings_1 << std::endl;
+
+			// copy in an other object using the copy assignment
+			strings_2 = strings_1;
+			// output the elements in strings_2
+			std::cout << strings_2 << std::endl;
+			// try to access with a valid index
+			std::cout << strings_1[2] << std::endl;
+			// try to access with an invalid index
+			std::cout << strings_2[5] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error:" << e.what() << std::endl;
+		}
+	}
+	
+    return (EXIT_SUCCESS);
+}
